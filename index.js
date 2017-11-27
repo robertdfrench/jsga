c8b64f1553b98e0d642fef01e3d11de4d6a62306
Robert D. French <frenchrd@ornl.gov>
2017-11-27 08:52:10 -0500

    Clarify purpose of each function

--- index.js
+++ index.js
@@ -4,39 +4,51 @@ var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 .
 
 
 function randomIndex(target) {
+	// Return a random integer from [0, target.length)
 	return Math.floor(Math.random() * target.length);
 }
 
 function randomCharacter() {
+	// Grab a random element from the alphabet
 	const index = randomIndex(alphabet);
 	return alphabet.charAt(index);
 }
 
 function createOrganism(target) {
+	// Generate a completely random organism of the target's length
 	const randoChars = charArray(target).map(x => randomCharacter());
 	return randoChars.join('');
 }
 
 function characterPosition(character) {
+	// Recover the index of each character relative to the alphabet
 	return alphabet.indexOf(character);
 }
 
 function zip(a, b) {
+	// Merge two arrays into a single array of pairs
 	return a.map((e,i) => [e, b[i]]);
 }
 
 function charArray(str) {
+	// Turns a string into a formal array of characters
 	return str.split('');
 }
 
 function distance(a, b) {
-	return Math.sqrt(zip(charArray(a), charArray(b)).
-		map(x => characterPosition(x[0]) - characterPosition(x[1])).
+	// Standard Euclidean distance metric
+	const aPositions = charArray(a).map(characterPosition);
+	const bPositions = charArray(b).map(characterPosition);
+	return Math.sqrt(
+		zip(aPoisitions, bPositions).
+		map(x => x[0] - x[1]).
 		map(x => x**2).
-		reduce((acc, cur) => acc + cur))
+		reduce((acc, cur) => acc + cur)
+	)
 }
 
 function clone(target) {
+	// Clone with entropy at a single random element
 	const index = randomIndex(target);
 	return charArray(target).
 		map((e, i) => i == index ? randomCharacter() : e).
